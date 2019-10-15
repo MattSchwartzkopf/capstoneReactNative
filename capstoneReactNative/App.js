db2ef9ff342b85fc8fef7793a0a4113a4c9712ff
MatthewSchwartzkopf <schwartzkopfm@gmail.com>
2019-10-15 17:25:46 -0400

    Pushing working code

--- capstoneReactNative/App.js
+++ capstoneReactNative/App.js
@@ -38,6 +38,7 @@ const App: () => React$Node = () => {
               <Text style={styles.footer}>Engine: Hermes</Text>
             </View>
           )}
+
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Step One</Text>
@@ -46,25 +47,31 @@ const App: () => React$Node = () => {
                 screen and then come back to see your edits.
               </Text>
             </View>
+
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>See Your Changes</Text>
               <Text style={styles.sectionDescription}>
                 <ReloadInstructions />
               </Text>
             </View>
+
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Debug</Text>
               <Text style={styles.sectionDescription}>
                 <DebugInstructions />
               </Text>
             </View>
+
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Learn More</Text>
               <Text style={styles.sectionDescription}>
                 Read the docs to discover what to do next:
               </Text>
             </View>
+
+            <Text>Extra data goes here</Text>
             <LearnMoreLinks />
+
           </View>
         </ScrollView>
       </SafeAreaView>
