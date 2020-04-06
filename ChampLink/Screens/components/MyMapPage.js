aa0102c597a08f15e612b3380fd51e6411640432
MattSchwartzkopf <schwartzkopfm@gmail.com>
2020-04-05 23:35:45 -0400

    Temp Implement with AirMapError(Brian)

--- ChampLink/Screens/components/MyMapPage.js
+++ ChampLink/Screens/components/MyMapPage.js
@@ -20,57 +20,31 @@ export default class App extends Component {
 
   render() {
     return (
+      <View style={styles.container}>
       <MapView
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    />
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      >
+      </MapView>
+    </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  main: {
-    flex: 1,
-    padding: 30,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    backgroundColor: '#005393',
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
-  title: {
-    paddingTop: 20,
-    marginBottom: 10,
-    fontSize: 25,
-    textAlign: 'center'
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
-  itemInput: {
-    height: 50,
-    padding: 4,
-    marginRight: 5,
-    fontSize: 23,
-    borderWidth: 1,
-    borderColor: 'white',
-    borderRadius: 8,
-    color: 'white'
-  },
-  buttonText: {
-    fontSize: 18,
-    color: '#111',
-    alignSelf: 'center'
-  },
-  button: {
-    height: 45,
-    flexDirection: 'row',
-    backgroundColor: 'white',
-    borderColor: 'white',
-    borderWidth: 1,
-    borderRadius: 8,
-    marginBottom: 10,
-    marginTop: 10,
-    alignSelf: 'stretch',
-    justifyContent: 'center'
-  }
-});
+ });
